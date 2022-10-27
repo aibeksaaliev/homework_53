8af4082d7a1f6e187cfb8ddbe795911165b20173
Aibek Saaliev <dokaidea@gmail.com>
2022-10-27 16:13:52 +0600

    created AddTaskForm component in AddTaskForm.tsx and added ccs for this component in AddTaskForm.css

---
+++ src/AddTaskForm/AddTaskForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './AddTaskForm.css';
+
+const AddTaskForm = () => {
+  return (
+    <form id="add_task_form">
+      <input className="task_input" type="text" placeholder="Add a new task"/>
+      <button className="add_btn" type="submit">Add</button>
+    </form>
+  )
+}
+
+export default AddTaskForm;
