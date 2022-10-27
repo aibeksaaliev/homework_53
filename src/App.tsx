2f6c3ea193dbcfe15b60f8a815b1d9eb7f24066a
Aibek Saaliev <dokaidea@gmail.com>
2022-10-27 16:44:50 +0600

    initiated state with a few default tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import AddTaskForm from "./AddTaskForm/AddTaskForm";
 import Task from "./Task/Task";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {key: "000001task", text: "Create AddTaskForum component."},
+    {key: "000002task", text: "Create Task component."}
+  ]);
+
+
   return (
     <div className="App">
       <AddTaskForm></AddTaskForm>
